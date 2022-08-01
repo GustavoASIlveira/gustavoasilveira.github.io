9abaa9256c75565350062c086e3894e35345beb6
gustavoASilveira <gustavo.nordheim@gmail.com>
2022-08-01 17:08:38 -0300

    eclipse atualizado

--- content/eclipse/js/script.js
+++ content/eclipse/js/script.js
@@ -1,56 +1,91 @@
-const cnv = document.querySelector('canvas')
+const $ = (tag) => document.querySelector(tag)
+
+const cnv = $('canvas')
+	cnv.width = innerWidth
+	cnv.height = innerHeight
 const ctx = cnv.getContext('2d')
-const txtScore = document.querySelector('#txtScore')
-const endModal = document.querySelector('#end-modal')
-const gameOverScore = document.querySelector('#game-over-score')
-const btnNewGame = document.querySelector('#btn-new-game')
-const startModal = document.querySelector('#start-modal')
-const startContainer = document.querySelector('.start-container')
-const musicIntro = document.querySelector('#music-intro')
-musicIntro.volume = .6
-const musicGame = document.querySelector('#music-game')
-musicGame.volume = .6
+const player = new Player(cnv.width/2,cnv.height/2,30,'#48FCFF')
+const shootingSpeed = 4
+const txtScore = $('#txtScore')
+const gameOverModal = $('#gameOverModal')
+const gameOverScore = $('#gameOverScore')
+const btnNewGame = $('#btnNewGame')
+const startModal = $('#startModal')
+const startContainer = $('#startContainer')
+const musicGame = $('#musicGame')
 const SHOOTING = 1
 const EXPLOSION = 2
-
-btnNewGame.addEventListener('click',init)
-
-cnv.width = innerWidth
-cnv.height = innerHeight
-
-const x = innerWidth/2
-const y = innerHeight/2
-
-const player = new Player(x,y,30,'#001')
 
 let projectiles = []
 let enemies = []
 let particles = []
-
-let animationId
-let intervalId
+let intervalID
+let animationID
 let score = 0
+
+function spawnEnemies(){
+	intervalID = setInterval(()=>{
+		const radius = Math.floor(Math.random() * 26) + 5
+		
+		let posX, posY
+		if(Math.random() < .5){
+			posX = Math.random() < .5 ? 0 - radius : cnv.width + radius
+			posY = Math.random() * cnv.height
+		} else {
+			posX = Math.random() * cnv.width
+			posY = Math.random() < .5 ? 0 - radius : cnv.height + radius
+		}
+		
+		const angle = Math.atan2(player.y - posY, player.x - posX)
+		const velocity = {
+			x: Math.cos(angle),
+			y: Math.sin(angle)
+		}
+		
+		const color = 'hsl('+ Math.random() * 360 +',50%,50%)'
+		
+		enemies.push(new Enemy(posX,posY,radius,color,velocity))
+	},1500)
+}
+
+cnv.addEventListener('click',(e)=>{
+	e.preventDefault()
+	playSound(SHOOTING)
+	const angle = Math.atan2(e.clientY - player.y, e.clientX - player.x)
+	const velocity = {
+		x: Math.cos(angle) * shootingSpeed,
+		y: Math.sin(angle) * shootingSpeed
+	}
+	projectiles.push(new Projectile(player.x, player.y, 3, '#48FCFF', velocity))
+})
 
 startContainer.addEventListener('click',()=>{
 	startModal.style.opacity = 0
-	init()
+	
+	
 	setTimeout(()=>{
 		startModal.style.zIndex = -1
 	},500)
+	
+	newGame()
 })
 
-cnv.addEventListener('click',(e)=>{
-	const angle = Math.atan2(e.clientY - y, e.clientX - x)
+btnNewGame.addEventListener('click', newGame)
+
+function loop(){
+	animationID = requestAnimationFrame(loop,cnv)
+	update()
+}
+
+function update(){
+	ctx.fillStyle = 'rgba(0,0,0,.1)'
+	ctx.fillRect(0,0,cnv.width,cnv.height)
 	
-	const velocity = {
-		x: Math.cos(angle) * 4,
-		y: Math.sin(angle) * 4
-	}
-	
-	playSound(SHOOTING)
-	
-	projectiles.push( new Projectile(x,y, 3, '#48FCFF', velocity) )
-})
+	checkEnemies()
+	checkProjectiles()
+	checkParticles()
+	player.update()
+}
 
 function playSound(soundType){
 	const sound = document.createElement('audio')
@@ -60,145 +95,105 @@ function playSound(soundType){
 	})
 }
 
-function loop(){
-	animationId = requestAnimationFrame(loop, cnv)
-	update()
-}
-
-function update(){
-	ctx.fillStyle = 'rgba(0,0,0,.1)'
-	ctx.fillRect(0, 0, cnv.width, cnv.height)
-	
-	checkBulletEnemyCollision()
-	
+function checkEnemies(){
 	enemies.forEach((enemy)=>{
 		enemy.update()
 		
-		const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-		
-		if(dist < player.radius + enemy.radius){
-			musicGame.pause()
-			musicGame.currentTime = 0
-			cancelAnimationFrame(animationId)
-			gameOverScore.innerText = score
-			endModal.style.opacity = 1
-			endModal.style.zIndex = 1
+		const distance = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+		if(distance < player.radius + enemy.radius){
+			gameOver()
 		}
 	})
-	
-	manageParticles()
-	
-	player.update()
 }
 
-
-function spawEnemies(){
-	intervalId = setInterval(()=>{
-		const radius = Math.floor(Math.random() * 26) + 5
-		let posX
-		let posY
-		
-		if(Math.random() < .5){
-			posX = Math.random() < .5 ? 0 - radius : cnv.width + radius
-			posY = Math.random() * cnv.height
-		} else {
-			posX = Math.random() * cnv.width
-			posY = Math.random() < .5 ? 0 - radius : cnv.height + radius
-		}
-		
-		const angle = Math.atan2(y - posY, x - posX)
-		
-		const color = `hsl(${Math.random() * 360},50%,50%)`
-		
-		const velocity = {
-			x: Math.cos(angle),
-			y: Math.sin(angle)
-		}
-		
-		enemies.push( new Enemy(posX, posY, radius, color, velocity) )
-	},1500)
+function gameOver(){
+	musicGame.pause()
+	musicGame.currentTime = 0
+	cancelAnimationFrame(animationID)
+	clearInterval(intervalID)
+	gameOverScore.innerText = score
+	gameOverModal.style.opacity = 1
+	gameOverModal.style.zIndex = 1
 }
 
-function checkBulletEnemyCollision(){
-	for(let pIndex = projectiles.length-1; pIndex >= 0; pIndex--){
-		const projectile = projectiles[pIndex]
-		projectile.update()
+function newGame(){
+	musicGame.play()
+	gameOverModal.style.opacity = 0
+	gameOverModal.style.zIndex = -1
+	projectiles = []
+	particles = []
+	enemies = []
+	score = 0
+	txtScore.innerText = 'SCORE: ' + score
+	loop()
+	spawnEnemies()
+	ctx.fillStyle = 'white'
+	ctx.fillRect(0,0,cnv.width,cnv.height)
+}
+
+function checkProjectiles(){
+	for(let i = projectiles.length -1; i >= 0; i--){
+		const p = projectiles[i]
+		p.update()
+		checkOffScreen(p,i)
 		
-		for(let eIndex = enemies.length-1; eIndex >= 0; eIndex--){
+		for(let eIndex = enemies.length -1; eIndex >= 0; eIndex--){
 			const enemy = enemies[eIndex]
-			const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+			const distance = Math.hypot(p.x - enemy.x, p.y - enemy.y)
 			
-			if(dist < projectile.radius + enemy.radius){
+			//Colisão do projétil com o inimigo
+			if(distance < p.radius + enemy.radius){
 				playSound(EXPLOSION)
-				score += 100 - Math.floor(enemy.radius)
-				txtScore.innerHTML = 'SCORE: ' + score
-				createParticles(projectile,enemy)
 			
 				if(enemy.radius > 15){
-					enemy.newRadius = enemy.radius - 10
+					enemy.newRadius = enemy.radius -10
 				} else {
 					enemies.splice(eIndex,1)
 				}
-				projectiles.splice(pIndex,1)
+				
+				score += 50 - Math.floor(enemy.radius)
+				txtScore.innerText = 'SCORE: ' + score
+				
+				projectiles.splice(i,1)
+				createParticles(enemy,p)
 			}
 		}
-		
-		checkOffScreenBullets(projectile,pIndex)
 	}
 }
 
-function checkOffScreenBullets(projectile,pIndex){
-	if(	projectile.x + projectile.radius < 0 || 
-		projectile.x - projectile.radius > cnv.width || 
+function checkOffScreen(projectile, index){
+	if(	projectile.x + projectile.radius < 0 ||
+		projectile.x - projectile.radius > cnv.width ||
 		projectile.y + projectile.radius < 0 || 
-		projectile.y + projectile.radius > cnv.height
-	){
+		projectile.y - projectile.radius > cnv.height)
+	{
 		score -= 100
 		if(score < 0){
 			score = 0
 		}
-		txtScore.innerHTML = 'SCORE: ' + score
-		projectiles.splice(pIndex,1)
+		
+		txtScore.innerText = 'SCORE: ' + score
+	
+		projectiles.splice(index,1)
 	}
 }
 
-function manageParticles(){
-	for(let prtIndex = particles.length-1; prtIndex >= 0; prtIndex--){
-		const particle = particles[prtIndex]
-		particle.update()
-		
-		if(particle.alpha <= 0){
-			particles.splice(prtIndex,1)
+function createParticles(enemy,projectile){
+	for(let i = 0; i < enemy.radius * 2; i++){
+		const velocity = {
+			x: (Math.random() - .5) * (Math.random() * 6),
+			y: (Math.random() - .5) * (Math.random() * 6)
+		}
+		particles.push(new Particle(projectile.x,projectile.y,Math.random()*2,enemy.color,velocity))
+	}
+}
+
+function checkParticles(){
+	for(let i = particles.length -1; i >= 0; i--){
+		const p = particles[i]
+		p.update()
+		if(p.alpha <= 0){
+			particles.splice(i,1)
 		}
 	}
 }
-
-function createParticles(projectile,enemy){
-	for(let i = 0; i < Math.floor(enemy.radius * 2); i++){
-		const velocity = {
-							x: (Math.random() - .5) * (Math.random() * 6),
-							y: (Math.random() - .5) * (Math.random() * 6)
-						}
-		particles.push(new Particle(projectile.x,projectile.y,Math.random() * 2,enemy.color,velocity))
-	}
-}
-
-function init(){
-	musicIntro.pause()
-	musicGame.pause()
-	musicGame.currentTime = 0
-	musicGame.play()
-	endModal.style.opacity = 0
-	endModal.style.zIndex = -1
-	projectiles = []
-	enemies = []
-	particles = []
-	animationId
-	score = 0
-	txtScore.innerHTML = 'SCORE: ' + score
-	clearInterval(intervalId)
-	ctx.fillRect(0,0,cnv.width,cnv.height)
-	spawEnemies()
-	loop()
-}
-
